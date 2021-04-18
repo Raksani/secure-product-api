65155838d1ed36379c4ace24ec6ca2ca8acbf8dc
Raksani <raksani.k@ku.th>
2021-04-18 17:12:11 +0700

    create auth for api service

--- src/main/java/ku/product/config/SecurityConfig.java
+++ src/main/java/ku/product/config/SecurityConfig.java
@@ -1,21 +1,52 @@
 package ku.product.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // create authentication manager bean to use with JWT
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/products").permitAll()
+//                .antMatchers("/api/products").permitAll()
+//                for a login request only. So, we need to authenticate before accessing to '/api/products'
+                .antMatchers("/api/auth/**").permitAll()
                 .anyRequest()
                 .authenticated();
+    }
+
+    // use password encryption: bcrypt
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(13);
+    }
+
+    // create in-memory user for authentication.
+    // (similar to the previous lab that used in-database user authentication
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("beau")
+                .password(encoder().encode("complexpassword"))
+                .roles("USER");
     }
 }
